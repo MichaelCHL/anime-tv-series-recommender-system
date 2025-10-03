2adfcbf62f9394ae7b8e91f9a50c5e091da3a303
Michael Work <michaelwork@Michaels-MacBook-Pro.local>
2025-10-03 16:53:01 +0800

    added year conversion function

--- src/utils/text_cleaning.py
+++ src/utils/text_cleaning.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -8,10 +9,20 @@ nltk.download('punkt')
 def stopword_remove(synopsis):
     
     # get english stopwords and tokenize 
-    stop_words = set(stop_words.words('english'))
+    stop_words = set(stopwords.words('english'))
     tokens = word_tokenize(synopsis.lower())
     
     # remove stopwords
     filtered_tokens = [word for word in tokens if word not in stop_words]
 
     return filtered_tokens
+
+def year_conversion(date_str):
+    year = re.findall(r"\b\d{4}\b", date_str)
+    try:
+        if not year:
+            return None
+        return year[0]
+    except IndexError as e:
+        raise(year, e) 
+
