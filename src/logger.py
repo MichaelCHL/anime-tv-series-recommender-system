1cfd9f9a47e8e032ecf63f6cf1d0404bc2fd4496
Michael Work <michaelwork@Michaels-MacBook-Pro.local>
2025-10-03 10:53:17 +0800

    added logger.py function

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,17 @@
-import logger
+import logging
 import os
+import datetime
 
 LOG_DIRS = 'logs'
 os.makedirs(LOG_DIRS, exist_ok=True)
+format = '%(asctime)s - %(levelname)s: %(messgage)s'
+
+logging.basicConfig(
+    filename=datetime.datetime.now().strftime('log_%Y-%m-%d.log')
+    level=logging.DEBUG,
+    format=format 
+)
+
+def logger(name):
+    return logging.getLogger(name)
+
