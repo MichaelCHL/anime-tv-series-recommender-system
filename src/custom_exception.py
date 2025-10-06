f8c08968d6a578f85100dea4e425ac22e888dfad
MichaelCHL <99242085+MichaelCHL@users.noreply.github.com>
2025-10-06 17:00:19 +0800

    Update custom_exception structure

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -6,3 +6,12 @@ class DataLoadingException(Exception):
 
     def __str__(self):
         return f"{self.message} (Error Code: {self.error_code})"
+    
+class DataCleaningException(Exception):
+    def __init__(self, message, error_code)
+        self.message = message
+        super().__init__(self.message)
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
