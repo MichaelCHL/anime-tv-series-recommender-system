231a674249ee3da738040fd874c59b1e9adb6b36
Michael Work <michaelwork@Michaels-MacBook-Pro.local>
2025-10-17 13:47:21 +0800

    finished tf-idf vect code

--- src/feature_engineering/tf_idf.py
+++ src/feature_engineering/tf_idf.py
@@ -1,17 +1,23 @@
 from utils.logger import get_logger
 from custom_exception import *
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from scipy.sparse import save_npz
+
 import pandas as pd
 import os
 import joblib
 
 logger = get_logger(__name__)
 
-class vertorization:
+class Vectorization:
     def __init__(self):
-        self.output_path = '../artifacts/processed/'
-        os.makedirs = os.makedirs(os.path.join(self.output_path, 'models'), exist_ok=True)
+        self.output_path = '../artifacts'
         self.data_path = os.path.join(self.output_path, 'processed')
+        self.model_path = os.path.join(self.output_path, 'models')
+        self.feature_path = os.path.join(self.output_path, 'features')
+        os.makedirs = os.makedirs(os.path.join(self.output_path, 'models'), exist_ok=True)
+        os.makedirs = os.makedirs(os.path.join(self.output_path, 'features'), exist_ok=True)
+
         self.df = None
 
     def load_data(self):
@@ -29,12 +35,26 @@ class vertorization:
             logger.info("Start tf-idf vectorization...")
             corpus = self.df['text_courpus']
             vectorizer = TfidfVectorizer()
-            X = vectorizer.fit_transform(corpus)
+            tfidf_matrix = vectorizer.fit_transform(corpus)
             
-            return vectorizer
+            model_file_path = os.path.join(self.model_path, 'tfidf_vectorizer.pkl')
+            joblib.dump(vectorizer, model_file_path)
+
+            feature_file_path = os.path.join(self.feature_path, 'tfidf_matrix.npz')
+            save_npz(feature_file_path, tfidf_matrix)
+            
+            logger.info("tf-idf vectorization completed successfully!")
+
 
         except Exception as e:
             logger.error("Failed to convert to td-idf!")
             raise VectorizationException("Failed to vectorize corpus", e)
 
-    
+    def run(self):
+        logger.info("Starting vectorization process...")
+        self.load_data()
+        self.td_idf()
+
+if __name__ == '__main__':
+    vectorizer = Vectorization()
+    vectorizer.run()
